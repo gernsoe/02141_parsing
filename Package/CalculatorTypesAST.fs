39fd35fd2e621d86dbe9977bc81fc6a62ea4fcf6
chris <christian.gernsoe@gmail.com>
2020-03-06 10:36:27 +0100

    AST finished

--- Package/CalculatorTypesAST.fs
+++ Package/CalculatorTypesAST.fs
@@ -1,14 +1,46 @@
+
 // This file implements a module where we define a data type "expr"
 // to store represent arithmetic expressions
-module CalculatorTypesAST
+module GCL_AST
 
-type expr =
-  | Num of float
-  | TimesExpr of (expr * expr)
-  | DivExpr of (expr * expr)
-  | PlusExpr of (expr * expr)
-  | MinusExpr of (expr * expr)
-  | PowExpr of (expr * expr)
-  | UPlusExpr of (expr)
-  | UMinusExpr of (expr)
-  | SqrtExpr of (expr)
+type x =
+    | xInt of int
+    | xString of string
+    | xChar of char
+
+type a =
+    | n of int
+    | X of x
+    | A of array<a>
+    | PlusExpr of (a * a)
+    | MinusExpr of (a * a)
+    | TimesExpr of (a * a)
+    | DivExpr of (a * a)
+    | UMinusExpr of (a)
+    | PowExpr of (a * a)
+
+type b =
+    | True of bool
+    | False of bool
+    | And1Expr of (b * b)
+    | Or1Expr of (b * b)
+    | And2Expr of (b * b)
+    | Or2Expr of (b * b)
+    | NotExpr of b
+    | EquExpr of (a * a)
+    | NequExpr of (a * a)
+    | Gt of (a * a)
+    | Ge of (a * a)
+    | Lt of (a * a)
+    | Le of (a * a)
+
+type C =
+  | AssignX of (x * a)
+  | AssignA of (a * a)
+  | Skip of string
+  | GoNext of (C * C)
+  | Iffi of GC
+  | Dood of GC
+and GC =
+  | Condition of (b * C)
+  | ElseIfExpr of (GC * GC)
