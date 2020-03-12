4b877da2a798cac58ed8ba3f052ef75ced0c55f6
Christian <christian.gernsoe@gmail.com>
2020-03-12 21:12:45 +0100

    Added comments

--- Package/tests.fsx
+++ Package/tests.fsx
@@ -1,4 +1,4 @@
-// This script implements our interactive parser
+// This script implements our test cases
 
 // We need to import a couple of modules, including the generated lexer and parser
 #r "FsLexYacc.Runtime.dll"
@@ -33,16 +33,44 @@ let parse lexbuf =
     // return the result of parsing (i.e. value of type "expr")
     res
     
+// Expected to be recognized
+let test1 = "if !true && false -> x := 5; x:=1 fi"
+
+// Expected to be recognized
+let test2 = "do true -> continue od"
+
+// Expected to be recognized
+let test3 = "y:=2;
+             do false -> y:= x*y;
+                       x:=x-1
+             od"
+        
+// Error expected
+let test4 = "y:=2;
+             do x>0 -> y:= x*y;
+                       x:=x-1 
+                          "
+
+// Expected to be recognized
+let test5 = "x:=2;
+            if true && !false -> x:= (((x*2)/2)+2-2)^2 
+            [] x != 2 -> x:= 2; A[x] := 1 fi"
+
 // We implement here the function that interacts with the user
-let rec compute =
+let rec compute n =
         printf "Enter an expression: "
         // translate string into a buffer of characters
-        let lexbuf = LexBuffer<_>.FromString (Console.ReadLine())
+        let lexbuf = LexBuffer<_>.FromString (n)
 
         // Gonna be used for a future pretty printer functionality
         let expression = parse lexbuf 
         // and print the result of evaluating it
         printfn "Grammar recognized" 
 
-// Start interacting with the user
-compute
+// Starting tests
+compute test1
+compute test2
+compute test3
+compute test4
+compute test5
+
